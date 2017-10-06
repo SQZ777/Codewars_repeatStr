a827fd7009a3af4f7a91e1506da6a0783753feff
darrenzhang <darrenzhang@91app.com>
2017-10-06 18:11:42 +0800

    init and create first test case

--- Codewars_repeatStr/UnitTest1.cs
+++ Codewars_repeatStr/UnitTest1.cs
@@ -6,8 +6,24 @@ namespace Codewars_repeatStr
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Input_0andstringNull_Should_Be_stringNull()
         {
+            var n = 0;
+            var s = string.Empty;
+
+            var act = Program.repeatStr(n, s);
+
+            var expected = string.Empty;
+
+            Assert.AreEqual(expected, act);
+        }
+    }
+
+    public class Program
+    {
+        public static string repeatStr(int i, string s)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
